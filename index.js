bfe9d47377ba09b0ac4650edaecde57ff23c0b0d
sanchitkhthpalia <114855470+sanchitkhthpalia@users.noreply.github.com>
2023-07-12 12:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for(var i=0;i<numberOfAlphabetButtons;i++){
     document.querySelectorAll(".alphabet")[i].addEventListener("click",function(){
         var buttonInnerHTML=this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keypress",function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -173,4 +175,11 @@ function makeSound(key){
         default:
             console.log();
     }
+}
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+      activeButton.classList.remove("pressed");
+  },100);
 }
